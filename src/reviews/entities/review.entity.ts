563e0e1ffc17be917847e0be3bbf83765dfef670
puppetzz <minhhieu.p3tz@gmail.com>
2023-05-30 12:14:57 +0700

    fix: return number type for rating

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -8,6 +8,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
+import { ColumnNumericTransform } from '../utils/column-numeric-tranformer';
 import { ReviewImage } from './review-image.entity';
 import { TripType } from './trip-type.entity';
 
@@ -17,11 +18,18 @@ export class Review {
   @ApiProperty()
   id: string;
 
-  @Column({ type: 'decimal', precision: 2, scale: 1, default: 0.0 })
+  @Column({
+    type: 'decimal',
+    precision: 2,
+    scale: 1,
+    default: 0.0,
+    nullable: false,
+    transformer: new ColumnNumericTransform(),
+  })
   @ApiProperty()
   rating: number;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   @ApiProperty()
   reviewAt: Date;
 
