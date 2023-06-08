78d6b73f83295cdd7de1257e7d8f6b4983fa6fab
Kaito <76529131+kaito-coder@users.noreply.github.com>
2023-06-08 10:25:39 +0700

    fix: add column for create room (#90)

--- db/migrations/1686150721338-add-paypal-payment.ts
+++ db/migrations/1686150721338-add-paypal-payment.ts
@@ -1,17 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddPaypalPayment1686145221178 implements MigrationInterface {
-    name = 'AddPaypalPayment1686145221178'
+export class AddPaypalPayment1686150721338 implements MigrationInterface {
+    name = 'AddPaypalPayment1686150721338'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "category_locations_location" DROP CONSTRAINT "FK_16c0a15a534e3f86e5dbf1757b9"`);
         await queryRunner.query(`CREATE TABLE "paypal" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "transactionId" character varying NOT NULL, "paidAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_7eb6ea66c59d87647756175458d" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "booking_room" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "numberOfRooms" integer NOT NULL, "roomId" uuid, "bookingId" uuid, CONSTRAINT "PK_e35dcb428979ee7cc7808440126" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "exchange_rate" ("id" SERIAL NOT NULL, "USD" integer NOT NULL DEFAULT '1', "VND" numeric NOT NULL, "date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_5c5d27d2b900ef6cdeef0398472" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "numberOfRooms"`);
-        await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "isFreeCancel"`);
         await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "freeCancelDueDate"`);
-        await queryRunner.query(`ALTER TABLE "hotel" DROP COLUMN "isRegistered"`);
+        await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "isFreeCancel"`);
+        await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "numberOfRooms"`);
         await queryRunner.query(`ALTER TABLE "booking" ADD "CustomerName" character varying`);
         await queryRunner.query(`ALTER TABLE "booking" ADD "totalAmount" integer NOT NULL`);
         await queryRunner.query(`ALTER TABLE "booking" ADD "isSuccess" boolean NOT NULL DEFAULT false`);
@@ -23,11 +21,9 @@ export class AddPaypalPayment1686145221178 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "booking" ADD CONSTRAINT "FK_4057539c990cefa802cf819c5d4" FOREIGN KEY ("paypalId") REFERENCES "paypal"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "booking_room" ADD CONSTRAINT "FK_50557fd862e2f80337e385433d4" FOREIGN KEY ("roomId") REFERENCES "room"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "booking_room" ADD CONSTRAINT "FK_d2c224e81b16a2c06aecfcd5842" FOREIGN KEY ("bookingId") REFERENCES "booking"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "category_locations_location" ADD CONSTRAINT "FK_16c0a15a534e3f86e5dbf1757b9" FOREIGN KEY ("locationId") REFERENCES "location"("id") ON DELETE NO ACTION ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "category_locations_location" DROP CONSTRAINT "FK_16c0a15a534e3f86e5dbf1757b9"`);
         await queryRunner.query(`ALTER TABLE "booking_room" DROP CONSTRAINT "FK_d2c224e81b16a2c06aecfcd5842"`);
         await queryRunner.query(`ALTER TABLE "booking_room" DROP CONSTRAINT "FK_50557fd862e2f80337e385433d4"`);
         await queryRunner.query(`ALTER TABLE "booking" DROP CONSTRAINT "FK_4057539c990cefa802cf819c5d4"`);
@@ -39,14 +35,12 @@ export class AddPaypalPayment1686145221178 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "isSuccess"`);
         await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "totalAmount"`);
         await queryRunner.query(`ALTER TABLE "booking" DROP COLUMN "CustomerName"`);
-        await queryRunner.query(`ALTER TABLE "hotel" ADD "isRegistered" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "booking" ADD "freeCancelDueDate" TIMESTAMP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "booking" ADD "isFreeCancel" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "booking" ADD "numberOfRooms" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "booking" ADD "isFreeCancel" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "booking" ADD "freeCancelDueDate" TIMESTAMP NOT NULL`);
         await queryRunner.query(`DROP TABLE "exchange_rate"`);
         await queryRunner.query(`DROP TABLE "booking_room"`);
         await queryRunner.query(`DROP TABLE "paypal"`);
-        await queryRunner.query(`ALTER TABLE "category_locations_location" ADD CONSTRAINT "FK_16c0a15a534e3f86e5dbf1757b9" FOREIGN KEY ("locationId") REFERENCES "location"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
 }
